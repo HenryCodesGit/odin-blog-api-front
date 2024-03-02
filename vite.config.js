a5e137ad8c5dfd44b635bb69abf4ef07f47a4bad
HenryCodesGit <153534511+HenryCodesGit@users.noreply.github.com>
2024-03-02 09:50:15 -0500

    Update vite config for deploy

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/odin-blog-api',
   plugins: [react()],
   test:{
     globals: true,
