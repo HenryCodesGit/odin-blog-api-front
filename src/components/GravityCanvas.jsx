365a4fa96182216f8171adf0ed3c2875415d7f3d
HenryCodesGit <153534511+HenryCodesGit@users.noreply.github.com>
2024-03-02 10:11:12 -0500

    Fixing more issues with config setup

---
+++ src/components/GravityCanvas.jsx
@@ -0,0 +1,19 @@
+// TODO: A Canvas element where i can enter in array of skills and it will create a canvas of floating orbs.
+// Each orb will float around with initial momentum and elastically collide with other orbs
+// Orbs of similar categories will have an attractive force
+// Orbs of differing categories will have a repulsive force
+// Make sure there's a maximum speed so that things don't get out of hand
+// The mouse will serve as a repulsive force
+
+/* See matter.js */
+// https://brm.io/matter-js/
+
+/*  Format of data input will be array of json objects [...Object]
+    Objects will be in the following format 
+    {
+        Description: HTML
+        ImageURL: https://example.com
+        Category: Webdev
+    }
+*/
+
