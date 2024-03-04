432d5f834b8e18256f89510232c37c44398c6d06
HenryCodesGit <153534511+HenryCodesGit@users.noreply.github.com>
2024-03-03 22:06:09 -0500

    Fix dependency issues causing unneeded function calls

--- src/components/NavDropdown.jsx
+++ src/components/NavDropdown.jsx
@@ -1,6 +1,6 @@
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
-import { useState, forwardRef, useEffect, useRef } from 'react';
+import { useState, forwardRef, useEffect, useRef, useCallback } from 'react';
 
 import style from '../styles/components/NavDropdown.module.css'
 
@@ -9,34 +9,50 @@ import style from '../styles/components/NavDropdown.module.css'
 import MenuIcon from '@mui/icons-material/Menu';
 
 const Component = forwardRef(function Component({ links }, ref){
-    // State to keep track of the menu items and when they are shown.
     const [isMenuShown, setMenuShown] = useState(false);
     const toggleMenu = () => setMenuShown(!isMenuShown);
 
-    const buttonRef = useRef(null);
+    const buttonHTML = useRef(null);
 
-    // Hides the menu if it loses focus
-    const checkHideMenu = (event) => {
-        if(event.target !== buttonRef.current && event.target !== buttonRef.current.querySelector('svg')){ 
-            setMenuShown(false) 
-        };
-    }
-    const listComponents = links.map(({id, title, url}) => <Link key= { id } to={ url }><li key={ id }>{ title }</li></Link>)
+    // Function to hides the menu if it loses focus
+    const checkHideMenu = useCallback((event) => {
+        // No point hiding menu if its already hidden
+        // Also don't hide the menu if clicking button. 
+        // The reason why is clicking the button will run 'toggleMenu' after this event runs which would then immediately turn on the menu again
+        if (!isMenuShown || event.target === buttonHTML.current || event.target === buttonHTML.current.querySelector('svg')) return;
 
+        setMenuShown(false) 
+    }, [isMenuShown]);
+
+    // Subscribe the hide menu function on every mouseUp click on the document
     useEffect(()=>{
         document.addEventListener('mouseup', checkHideMenu);
-        return ()=>{
-            document.removeEventListener('mouseup',checkHideMenu)
-        }
-    }, []);
+
+        return ()=>{ document.removeEventListener('mouseup',checkHideMenu); }
+    }, [checkHideMenu]);
     
-    return(<div className={style.floatingContainer} ref={ref}><nav className={style.nav}>
-        <button className={style.button} onClick={ toggleMenu } ref={buttonRef}><MenuIcon /></button>
-        <ul className={ `${style.ul} ${isMenuShown ? '' : style.visuallyHidden}` }>
-            {listComponents}
-        </ul>
-    </nav></div>);
-})
+    // Map list items to components
+    const listComponents = links.map(({id, title, url}) =>
+        <li key={ id } tabIndex='-1'>
+            <Link to={ url } tabIndex={isMenuShown ? 0 : -1}>
+                { title }
+            </Link>
+        </li>
+    );
+
+    return(
+        <div className={style.floatingContainer} ref={ref}>
+            <nav className={style.nav}>
+                <button className={style.button} onClick={ toggleMenu } ref={buttonHTML} aria-expanded={isMenuShown} aria-haspopup="true">
+                    <MenuIcon />
+                </button>
+                <ul className={ `${style.ul} ${isMenuShown ? '' : style.visuallyHidden}` } tabIndex='-1'>
+                    { listComponents }
+                </ul>
+            </nav>
+        </div>
+    );
+});
 
 Component.propTypes = {
     links: // Array of links to be generated into the nav bar 
--- src/utilities/react-utils/useResizeEffect.js
+++ src/utilities/react-utils/useResizeEffect.js
@@ -1,6 +1,6 @@
 /* This is a custom hook made to do something whenever window is resized */
 
-import { useEffect, useRef} from "react"
+import { useEffect, useCallback } from "react"
 import debounce from "../debounce";
 
 const defaultOptions = {
@@ -8,26 +8,34 @@ const defaultOptions = {
     debounce: 30
 }
 
-export default function useResizeEffect(fn, dependencies, options = defaultOptions){    
-    const callback = useRef(null);
-
+export default function useResizeEffect(fn, cleanup, dependencies, options){    
     //Over-write default options with any specifics given by the user
     options = Object.assign(defaultOptions, options);
 
-    //Run the callback at least once when the hook is loaded and option is provided
-    useEffect(()=>{
-        if(options.runInitial) window.dispatchEvent(new Event('resize'));
-    },[options]);
+    //Disabling linter rule because responsibility for dependencies is passed to 'dependencies' variable
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    const memofn = useCallback(fn, dependencies);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    const memoCleanUp = useCallback(cleanup, dependencies);
 
     //Apply debounce to the callback, if it is provided
     useEffect(() => {
-        callback.current = callback.current || (Object.hasOwn(options,'debounce') ? debounce(fn, options.debounce) : fn);
+        const callback = (Object.hasOwn(options,'debounce') ? debounce(memofn, options.debounce) : memofn);
 
         /* Add event listener */
-        window.addEventListener('resize',callback.current)
+        window.addEventListener('resize',callback)
 
-        return () => { window.removeEventListener('resize',callback.current);}
+        return () => { 
+            window.removeEventListener('resize',callback);
+            memoCleanUp();
+        }
     // Lint disable because it doesn't like spread array dependencies??
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [fn, options, ...dependencies])
+    }, [memofn, memoCleanUp, options]) /* [fn, options, ...dependencies] */
+
+    //Run the callback at least once when the hook is loaded and option is provided
+    useEffect(()=>{
+        if(options.runInitial){
+            window.dispatchEvent(new Event('resize'));
+        };
+    },[options]);
 }
